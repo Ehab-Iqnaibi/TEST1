5a2948c2427a814f36c1fd636af140023cd450c6
Ehab-Iqnaibi <iqnaibi_ehab@ppu.edu>
2022-10-15 00:00:56 +0300

    second

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-PRIN
+input_text = """
+One day a rabbit was boasting about,how fast he could run.
+He was laughing at the turtle for being so slow. Much to the
+rabbit’s surprise, the turtle challenged him to a race. The rabbit
+thought this was a good joke and accepted the challenge. The fox
+was to be the umpire of the race. As the race began, the rabbit
+raced way ahead of the turtle, just like everyone thought.
+The rabbit got to the halfway point and could not see the turtle
+anywhere. He was hot and tired and decided to stop and take a short
+nap. Even if the turtle passed him, he would be able to race to the
+finish line ahead of him. All this time the turtle kept walking step
+by step by step. He never quit no matter how hot or tired he got.
+He just kept going.
+However, the rabbit slept longer than he had thought and woke up.
+He could not see the turtle anywhere! He went at full speed to
+the finish line but found the turtle there waiting for him.
+""".lower()
+"""
+TODO:
+1. unify case, i.e., ==> lower done
+2. remove irrelevant words, the, a, is, was,
+3. remove propositions
+"""
+irrelevant_words = {".", "," , "\n", " the ", " a ", " and ",
+" is ", " was ", " he ", " at ", " to ", " for ", " can ", " this ", " we ", " you "
+                    , " or ", " i ", " an ", " but ", " so ", " yes ", " no "}
+for iw in irrelevant_words:
+  input_text = input_text.replace(iw, " ")
+print(input_text)
+word_list = input_text.split(" ")
+print(word_list)
+word_histogram = {
+}
+for word in word_list:
+  if word not in word_histogram.keys():
+    word_histogram[word] = 1
+  else:
+    word_histogram[word] = word_histogram[word] + 1
+word_histogram.pop("")
+print(word_histogram)
